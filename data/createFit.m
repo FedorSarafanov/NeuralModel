9dacce6f3fc001fc3e08c97b9c625d81fbef5be5
Fedor Sarafanov <sfg180@yandex.ru>
2018-03-20 23:09:34 +0300

    Правочки

--- data/createFit.m
+++ data/createFit.m
@@ -7,7 +7,7 @@
 % y=fitresult(x);
 %y=fittedmodel4(x);
 
-fid = fopen('/home/lab/neiro/data/matlab_r4c.tsv', 'wt+');
+fid = fopen('/home/lab/neiro/data/berst2.tsv', 'wt+');
 fprintf(fid, 't\tu\tv\tx\ty\n','');
 for i=1:1:length(t)
     fprintf(fid, '%g\t%g\t%g\t%g\t%g\n', [t(i) u(i) v(i) x(i) y(i)]);
---
+++ ku.m
@@ -0,0 +1,7 @@
+function [ u ] = ku( s )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
